cc73d8bb1db06e54114351ecc3e0d584a6c9852d
peleg <peleg.reichman@mail.huji.ac.il>
2022-04-22 12:59:04 +0300

    Ex1

--- exercises/house_price_prediction.py
+++ exercises/house_price_prediction.py
@@ -1,12 +1,16 @@
+import os
+
 from IMLearn.utils import split_train_test
 from IMLearn.learners.regressors import LinearRegression
-
-from typing import NoReturn
+from IMLearn.metrics.loss_functions import mean_square_error
+from typing import NoReturn, Tuple, Dict
 import numpy as np
 import pandas as pd
+from pandas import DataFrame
 import plotly.graph_objects as go
 import plotly.express as px
 import plotly.io as pio
+
 pio.templates.default = "simple_white"
 
 
@@ -23,7 +27,29 @@ def load_data(filename: str):
     Design matrix and response vector (prices) - either as a single
     DataFrame or a Tuple[DataFrame, Series]
     """
-    raise NotImplementedError()
+    hp_df: DataFrame = pd.read_csv(filename)
+
+    # Drop null values and duplicate samples
+    hp_df.dropna(inplace=True)
+    hp_df.drop_duplicates(inplace=True)
+
+    # Edit features to match thier true range.
+    hp_df['date'] = hp_df['date'].str[:4]
+    hp_df = hp_df[hp_df['bedrooms'].isin(range(20))]
+    hp_df = hp_df[hp_df['bathrooms'].isin(range(9))]
+    hp_df = hp_df[hp_df['waterfront'].isin(range(2))]
+    hp_df = hp_df[hp_df['view'].isin(range(5))]
+    hp_df = hp_df[hp_df['condition'].isin(range(1, 6))]
+    hp_df = hp_df[hp_df['grade'].isin(range(1, 14))]
+
+    # For categorical features create dummies.
+    hp_df = pd.get_dummies(hp_df,
+                           columns=['zipcode', 'yr_built', 'yr_renovated', 'lat', 'long'],
+                           )
+
+    # Drop uncorrelated features.
+    hp_df.drop(columns=['id', 'date'], inplace=True)
+    return hp_df.drop(columns=['price']), hp_df['price']
 
 
 def feature_evaluation(X: pd.DataFrame, y: pd.Series, output_path: str = ".") -> NoReturn:
@@ -43,19 +69,37 @@ def feature_evaluation(X: pd.DataFrame, y: pd.Series, output_path: str = ".") ->
     output_path: str (default ".")
         Path to folder in which plots are saved
     """
-    raise NotImplementedError()
+
+    for feature_name in X:
+        feature: pd.Series = X[feature_name]
+        cov: np.ndarray = np.cov(feature, y)
+        p_corr: np.ndarray = cov / (feature.std() * y.std())
+
+        data = DataFrame({feature_name: feature, 'response': y})
+        # layout1 = dict(title=dict(text="A Figure Specified By A Graph Object"))
+        fig = px.scatter(data, x=feature_name, y='response',
+                         title=f"Pearson Correlation between feature {feature_name} "
+                               f"and the response vector Prices: <br>"
+                               f"<b> with value {p_corr[0, 1].round(5)} <b>")
+
+        fig.update_layout(xaxis_title=f"feature {feature_name}", yaxis_title="Response vector - Prices")
+        fig.write_image(os.path.join(output_path + f'feature_{feature_name}_corr.png'))
 
 
 if __name__ == '__main__':
     np.random.seed(0)
     # Question 1 - Load and preprocessing of housing prices dataset
-    raise NotImplementedError()
+    observations, response = load_data('..\\datasets\\house_prices.csv')
+    # print(observations)
+    # print('\n --------------------- \n')
+    # print(response)
 
     # Question 2 - Feature evaluation with respect to response
-    raise NotImplementedError()
+    out_dir_path = 'C:\\Users\\peleg\\Desktop\\university\\year C\\B\\IML\\Exercises\\Ex2\\output Ex2\\'
+    feature_evaluation(observations, response, output_path=out_dir_path)
 
     # Question 3 - Split samples into training- and testing sets.
-    raise NotImplementedError()
+    # x_train, y_train, x_test, y_test = split_train_test(observations, response, train_proportion=0.75)
 
     # Question 4 - Fit model over increasing percentages of the overall training data
     # For every percentage p in 10%, 11%, ..., 100%, repeat the following 10 times:
@@ -64,4 +108,13 @@ if __name__ == '__main__':
     #   3) Test fitted model over test set
     #   4) Store average and variance of loss over test set
     # Then plot average loss as function of training size with error ribbon of size (mean-2*std, mean+2*std)
-    raise NotImplementedError()
+    # mse: Dict[int, float] = {}
+    # for p in range(10, 101):
+    #     px_train = x_train.sample(frac=float(p/100)).to_numpy()
+    #     py_train = y_train.sample(frac=float(p / 100)).to_numpy()
+    #     model = LinearRegression(include_intercept=True).fit(px_train, py_train)
+    #
+    #     y_predict = model.predict(px_train)
+    #     mse[p] = mean_square_error(py_train, y_predict)
+
+    # TODO: Plot!.
